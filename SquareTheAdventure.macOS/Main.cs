5999dce3e09c631415bf5abb2cac1fc32b152774
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2018-04-30 22:41:37 -0300

    Rename to SquareTheAdventure

---
+++ SquareTheAdventure.macOS/Main.cs
@@ -0,0 +1,37 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using AppKit;
+using Foundation;
+
+using Hydra;
+#endregion
+
+namespace SquareTheAdventure.macOS
+{
+    class Program : NSApplicationDelegate
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+            NSApplication.SharedApplication.Delegate = new Program();
+
+			using (var game = new Game1())
+			{
+				game.Run();
+			}
+
+            MemoryCard.current.saveGame(true);
+        }
+
+		public override void WillResignActive(NSNotification notification)
+		{
+            MemoryCard.current.saveGame();
+		}
+	}
+}
