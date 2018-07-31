4d85595239c126f1cab260d8ebbd9eed8f6b30d8
Pablo Henrique Bertaco <pablo_fonseca91@icloud.com>
2018-07-30 22:22:11 -0300

    Add GameColors fontWhite

--- SquareTheAdventure/SquareTheAdventure/GameColors.cs
+++ SquareTheAdventure/SquareTheAdventure/GameColors.cs
@@ -8,6 +8,7 @@ namespace Hydra
     {
         internal static Color backgroundColor = new Color(0.0862745098f, 0.05882352941f, 0.1490196078f);
 
-        internal static Color fontBlack = new Color(0, 0, 0);
+        internal static Color fontBlack = new Color(0.0f, 0.0f, 0.0f);
+        internal static Color fontWhite = new Color(1.0f, 1.0f, 1.0f);
     }
 }
